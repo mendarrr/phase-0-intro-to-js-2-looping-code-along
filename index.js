0fc9d97b72ffc78d5bfaf52f154c29b0672a80ca
mendarrr <sarahabby541@gmail.com>
2024-03-22 22:40:35 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const message = "surprise";
+
+function writeCards(names, message) {
+  return names.map(name => `Thank you, ${name}, for the wonderful ${message} gift!`);
+}
+
+module.exports = {
+  writeCards
+};
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  module.exports = {
+    countDown
+  };
